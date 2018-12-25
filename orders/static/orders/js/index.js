75e59369c91653bd7cc6a842479a9504296664f0
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2018-12-25 14:29:09 -0200

    returning origial select items setting

--- orders/static/orders/js/index.js
+++ orders/static/orders/js/index.js
@@ -21,7 +21,6 @@ document.addEventListener('DOMContentLoaded', () => {
 						button.disabled = false;
 						button.title = "Remove from the cart";
 						button.classList.add("itemIsSelected");
-						button.style.color = "#f17060"
 						button.dataset.operation = "remove";
 					},
 					failure: function(data) { 
@@ -42,7 +41,6 @@ document.addEventListener('DOMContentLoaded', () => {
 						debugger
 						button.title = "Add to the cart";
 						button.classList.remove("itemIsSelected");
-						button.style.color = "#fff"
 						button.dataset.operation = "add";
 					},
 					failure: function(data) { 
